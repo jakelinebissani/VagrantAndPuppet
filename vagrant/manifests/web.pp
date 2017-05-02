d6d17954e4ee491e7e08558d4e59ecc2f9165aec
Unknown <jakeline.bissani@gmail.com>
2017-05-02 02:26:47 -0300

    provisioning

--- vagrant/manifests/web.pp
+++ vagrant/manifests/web.pp
@@ -2,7 +2,7 @@ exec { "apt-update":
     command => "/usr/bin/apt-get update"
 }
 
-package { ["openjdk-7-jre", "tomcat7"]:
+package { ["openjdk-7-jre", "tomcat7", "mysql-server"]:
     ensure => installed,
     require => Exec["apt-update"]
 } 
@@ -15,6 +15,21 @@ service { "tomcat7":
     require => Package["tomcat7"]
 }
 
+service { "mysql":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package["mysql-server"]
+}
+
+exec { "musicjungle":
+    command => "mysqladmin -uroot create musicjungle",
+    unless => "mysql -u root musicjungle",
+    path => "/usr/bin",
+    require => Service["mysql"]
+}
+
 file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     source => "/vagrant/manifests/vraptor-musicjungle.war",
     owner => tomcat7,
@@ -22,4 +37,11 @@ file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     mode => 0644,
     require => Package["tomcat7"],
     notify => Service["tomcat7"]
+}
+
+
+define myfile_line($file, $line) {
+    exec { "/bin/echo '${line}' >> '${file}'":
+        unless => "/bin/grep -qFx '${line}' '${file}'"
+    }
 }
